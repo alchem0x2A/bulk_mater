7827282bb33a6db6f48bdf7bc7bbb68f9b10cc11
Tian Tian <tian.tian@chem.ethz.ch>
2018-11-26 11:36:27 +0100

    Add more tests for build script

--- tests/basic/test_build.py
+++ tests/basic/test_build.py
@@ -11,11 +11,15 @@ from ase.parallel import parprint
 
 class TestPath(unittest.TestCase):
     def test_build_entries(self):
+        """Get all entries from StructureBuilder
+        """
         sb = StructureBuilder()
         entries = sb.entries
         # [print(l) for l in entries]
         self.assertGreater(len(entries), 0)
     def test_simple_build(self):
+        """Generate all materials within the entries
+        """
         sb = StructureBuilder()
         entries = sb.entries
         failed = []
@@ -32,6 +36,8 @@ class TestPath(unittest.TestCase):
             parprint("All structure passed!")
         self.assertEqual(len(failed), 0)
     def test_build_by_name(self):
+        """Build materials by name and prototype
+        """
         sb = StructureBuilder()
         test_samples = [("AlAs", "Z"),
                         ("ZnO", "z"),
@@ -56,5 +62,28 @@ class TestPath(unittest.TestCase):
         else:
             parprint("All structure passed!")
         self.assertEqual(len(failed), 0)
+    def test_index(self):
+        """Are indexes from 2 instances the same?
+        """
+        sb1 = StructureBuilder()
+        sb2 = StructureBuilder()
+        len1 = sb1.count
+        len2 = sb2.count
+        self.assertEqual(len1, len2)
+        diff = [i for i in range(len1) \
+                if sb1.entries[i] != sb2.entries[i]]
+        # [print(sb1.entries[i], sb2.entries[i]) for i in range(len1)]
+        self.assertEqual(len(diff), 0)
+
+    def test_index(self):
+        """Materials from index?
+        """
+        # Are indexes from 2 instances the same?
+        sb = StructureBuilder()
+        n = sb.count
+        maters = sb.from_index(range(n))
+        failed = [m for m in maters if False in m.pbc]
+        self.assertEqual(len(failed), 0)
+        # [print(sb1.entries[i], sb2.entries[i]) for i in range(len1)]
 if __name__ == "__main__":
     unittest.main()
