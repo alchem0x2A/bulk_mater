86130ca055730f61e0acb7d709ecb7ce50712c7f
Tian Tian <tian.tian@chem.ethz.ch>
2018-12-07 10:32:18 +0100

    Add test for all class method (relax+BG+dielectric)

--- tests/bandgap/test_class_method.py
+++ tests/bandgap/test_class_method.py
@@ -14,24 +14,22 @@ class Test(unittest.TestCase):
         sb = StructureBuilder()
         atoms, *_ = sb.get_structure("C", "diamond")
         base_dir = os.path.join(os.path.dirname(__file__),
-                                "../../tmp/Si-module-test/")
+                                "../../tmp/C-module-test/")
         m_calc = MaterCalc(atoms=atoms, base_dir=base_dir)
         self.assertTrue(m_calc.relax(fmax=0.002))
         self.assertTrue(m_calc.ground_state())
         # PBE
         Eg_min, Eg_dir, bs = m_calc.bandgap("PBE")
+        Eg_gllb_min, Eg_gllb_dir, bs = m_calc.bandgap("GLLB")
         with open("PBE-gap.npz", "wb") as f:
             numpy.savez(f, Eg_min=Eg_min,
                         Eg_dir=Eg_dir,
                         **bs)
+        parprint("{:.3f}\t{:.3f}".format(Eg_min, Eg_dir))
         parprint("PBE Gap: min \t dir")
-        parprint("{:.3f}\t{:.3f}".format(Eg_min, Eg_dir))
-        parprint("Bandstructure PBE:", bs)
         # GLLB
-        Eg_min, Eg_dir, bs = m_calc.bandgap("GLLB")
         parprint("GLLB Gap: min \t dir")
-        parprint("{:.3f}\t{:.3f}".format(Eg_min, Eg_dir))
-        parprint("Bandstructure PBE:", bs)
+        parprint("{:.3f}\t{:.3f}".format(Eg_gllb_min, Eg_gllb_dir))
         
         
 
